6f8d91f256ad88cb0e46b850a3d0f5b790b97d59
jonathan <galactisphere@users.noreply.github.com>
2020-02-28 01:01:24 +1100

    Create metro.py
    Create layout for project

---
+++ metro.py
@@ -0,0 +1,10 @@
+# Obtain input data
+
+    # 
+    # Width for cross passages
+
+# Calculate volume of crushed rock and spoil which will be removed
+
+# Calculate weight of crushed rock
+
+# Calculate number of trucks required for transport
