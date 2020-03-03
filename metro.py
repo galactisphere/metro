4ecbc59562f0edb8c756b570b6e9836719cad468
jonathan <galactisphere@users.noreply.github.com>
2020-03-03 12:06:08 +1100

    Added function to calculate volume of rock

--- metro.py
+++ metro.py
@@ -1,3 +1,5 @@
+import math
+
 # Obtain input data
 
     # 
@@ -5,6 +7,25 @@
 
 # Calculate volume of crushed rock and spoil which will be removed
 
+def calculateRock(passageWidth):
+    return(
+        # Round to 2 decimal places
+        round(
+            # Multiply by 8 stations and add 10%
+            8 * 1.1 * (
+                # Calculate tunnel
+                float(math.pi) * math.sqrt(3) * 16500 +
+                # Calculate cross passages
+                16500 / 240 * 8 * 3.5 * float(passageWidth)
+            )
+        , 2)
+    )
+
+print(calculateRock(input()))
+
 # Calculate weight of crushed rock
+
+# def calculateRockWeight():
+
 
 # Calculate number of trucks required for transport
